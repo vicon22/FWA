898587f21fdeb9093cd74b4a7cb5a73bb4be6bc6
Evelyn Veiled <eveiled@pr-d3.kzn.21-school.ru>
2022-05-26 16:38:01 +0300

    updateSQLschemas

--- Cinema/src/main/resources/sql/schema.sql
+++ Cinema/src/main/resources/sql/schema.sql
@@ -1,9 +1,21 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS sesions;
+
 CREATE TABLE users(
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     email varchar(50) NOT NULL UNIQUE,
     firstName varchar(50) NOT NULL,
     lastName varchar(50) NOT NULL,
     phone varchar(50) NOT NULL,
     password varchar(250) NOT NULL
 );
+
+CREATE TABLE sessions(
+    session_id SERIAL,
+    user_id int NOT NULL,
+    ip varchar(250) NOT NULL,
+    date varchar(250) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
